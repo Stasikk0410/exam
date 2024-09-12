75702b0c6062b38007b6ff344b38731ae6515609
Stasikk0410 <92378083+Stasikk0410@users.noreply.github.com>
2024-09-12 18:43:10 +0300

    Add files via upload

--- exam/script.js
+++ exam/script.js
@@ -1,4 +1,3 @@
-// Toggle theme button
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
 
@@ -6,6 +5,5 @@ themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
 
-// Set the current year in footer
 const year = new Date().getFullYear();
 document.getElementById('year').textContent = year;
